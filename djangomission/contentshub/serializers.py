90172d38ed1edb3ca9c168ff6aec50b3ec11f5ad
gshduet <modis2199@gmail.com>
2022-10-02 21:20:17 +0900

    Fix:Put Authorized

--- djangomission/contentshub/serializers.py
+++ djangomission/contentshub/serializers.py
@@ -1,26 +1,25 @@
 from rest_framework import serializers
 
 from .models import Master, Klass
-from accounts.models import User
 
 
 class SetMasterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Master
-        fields = ['master_name','description']
+        fields = ['master_name', 'description']
 
 
 class KlassCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Klass
-        fields = ['title', 'description'] 
+        fields = ['title', 'description']
 
 
-class KlassRetriveSerializer(serializers.ModelSerializer):
+class KlassDetailSerializer(serializers.ModelSerializer):
     master_name = serializers.ReadOnlyField(source='master.master_name')
 
     class Meta:
         model = Klass
-        fields = ['master','master_name', 'title', 'description']
+        fields = ['master', 'master_name', 'title', 'description']
